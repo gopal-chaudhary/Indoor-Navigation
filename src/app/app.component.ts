f622276fe46bbeb9e6a376a8df6208ead8814979
GopalChaudh <kc7198931@gmail.com>
2024-09-06 07:11:15 +0530

    Add existing project files to Git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MapComponent } from "./map/map.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
