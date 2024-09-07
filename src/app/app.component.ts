8f71df586a0110c80ed6d4e672eefbb28ceec65e
GopalChaudh <kc7198931@gmail.com>
2024-09-07 12:27:15 +0530

    edit page added but with some errors lol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MapComponent } from './component/map/map.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MapComponent],
+  imports: [MapComponent,RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/pages/edit-map-page/edit-map-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { EditMapComponent } from '../../component/edit-map/edit-map.component';
+
+@Component({
+  selector: 'app-edit-map-page',
+  standalone: true,
+  imports: [EditMapComponent],
+  templateUrl: './edit-map-page.component.html',
+  styleUrl: './edit-map-page.component.scss'
+})
+export class EditMapPageComponent {
+
+}
