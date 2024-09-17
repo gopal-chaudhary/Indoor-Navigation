5c9e9478a83e0c4c79f3e0bc6ed62374ad300410
rakshit <rakshitambi7a@gmail.com>
2024-09-18 02:13:45 +0530

    added a loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RailwayLoaderComponent } from './component/loader/loader.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, RailwayLoaderComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
